1064db09f87ed9505ce31729f502f22f2d07d6f0
Yamato <aizawayamato@aizawa-no-MacBook-Air.local>
2018-10-25 23:43:55 +0900

    [WIP] implement eventsShow

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,13 @@ class EventsShow extends Component {
     this.onDeleteClick = this.onDeleteClick.bind(this);
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    if (id) {
+      this.props.getEvent(id);
+    }
+  }
+
   renderField(field) {
     const { input, label, type, meta: { touched, error } } = field;
 
@@ -65,8 +72,16 @@ const validate = values => {
   return errors;
 };
 
-const mapDispatchToProps = ({ deleteEvent });
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id];
+  return { initialValues: event,  }
+}
+const mapDispatchToProps = ({ deleteEvent, getEvent });
 
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({ 
+    validate, 
+    form: 'eventShowForm', 
+    enableReinitialize: true
+   })(EventsShow)
 );
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,13 +1,17 @@
 import _ from 'lodash';
 import {
   READ_EVENTS,
-  DELETE_EVENT
+  DELETE_EVENT,
+  READ_EVENT
 } from '../actions';
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id');
+    case READ_EVENT:
+      const data = action.response.data;
+      return { ...events, [data.id]: data }
     case DELETE_EVENT:
       delete events[action.id];
       return { ...events };
